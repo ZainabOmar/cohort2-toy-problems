386405f95529a27e3ce91543ba7f8beea77e5ed0
Zainab Hammami <zainab.hammami.zh@gmail.com>
2017-01-26 09:30:27 +0200

    binary

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -10,7 +10,30 @@
  * console.log(index); // 4
  */
 
-var binarySearch = function (array, target) {
-  
+var binarySearch1 = function (array, target) {
+  for (var i = 0; i < array.length; i++) {
+  	if (array[i] === target) {
+  		return i;
+  	}
+  }
 };
 
+
+var binarySearch2 = function (array, target) {
+	// var maxInd = array.length-1;
+	// var minInd = 0;
+	// var midInd = Math.floor(maxInd-minInd/2);
+	//var arr1 = x.splice(Math.floor(x.length/2), Math.ceil(x.lenght/2))
+	var arr2 = Math.floor(array.length/2);
+	for (var i = 0; i < arr1.length; i++) {
+		if (arr1[i]===target) {
+			return i;
+		}
+	}
+	for (var j = 0; j < arr2.length; j++) {
+		if (arr2[j] === target) {
+			return j;
+		}
+	}
+	return -1;
+};
