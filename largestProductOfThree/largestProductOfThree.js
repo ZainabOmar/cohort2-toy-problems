e0488f83bac2ca7b90ed84a52bc2634e69352464
Zainab Hammami <zainab.hammami.zh@gmail.com>
2017-01-31 09:28:25 +0200

    largest product of three

--- largestProductOfThree/largestProductOfThree.js
+++ largestProductOfThree/largestProductOfThree.js
@@ -7,10 +7,23 @@
  * Extra credit: Make your function handle negative numbers.
  */
 
+
+
 var largestProductOfThree = function(array) {
-	
+	var result = 1;
+	var x = 0;
+	var counter = 0;
+	for (var i = 0; i < array.length; i++) {
+		if (array[i] < 0) {
+			counter++;
+		}
+	}
+	if (counter === 0 || counter === array.length) {
+		for (var i = 3; i > 0; i--) {
+			x = Math.max(...array)
+			result = result * x
+			array.splice(array.indexOf(x))
+		}
+	}
+	return result;
 };
-
-
-
-
