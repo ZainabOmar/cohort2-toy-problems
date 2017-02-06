773aaf887f5219d7e4b860f148cadbbc7705538d
Zainab Hammami <zainab.hammami.zh@gmail.com>
2017-02-06 09:00:24 +0200

    prime numbers

--- primeTester/primeTester.js
+++ primeTester/primeTester.js
@@ -4,7 +4,24 @@
  * a prime number, false if it's not.
  */
 var primeTester = function(n) {
+ for (var i = 2; i < n; i++) {
+  	if(n%i===0){
+  		return true
+  	}
+  } 
+ return false;
+}
 
+
+var generate = function (range) {
+	var array = [];
+	var temp = 1;
+	for (var i = 1; i <= range; i++) {
+			if(primeTester(i)===false) {
+				array.push(i)
+			}
+		}
+	return array;
 }
 
  /* Extra credit: Write a function that generates a list of all prime numbers
